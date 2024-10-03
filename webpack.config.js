a895e08a06cb069dd1723e63cb46d0d8129cc632
Pedrooaj <pedroantonio5735@gmail.com>
2024-10-03 11:33:10 -0300

    webpack to type: module

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: 'production',
+  entry: './frontend/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }]
+  },
+  devtool: 'source-map'
+};
